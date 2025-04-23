8c0bdbf30a19e1ed56332884022e00b389c9f551
yamahand <a18293372@gmail.com>
2025-04-23 14:27:38 +0900

    テストの引数変更

--- run_tests.bat
+++ run_tests.bat
@@ -23,9 +23,9 @@ if errorlevel 1 (
 
 REM テスト実行
 if exist Debug\test_all.exe (
-    Debug\test_all.exe -v high -s -r junit
+    Debug\test_all.exe -v high
 ) else if exist test_all.exe (
-    test_all.exe -v high -s -r junit
+    test_all.exe -v high
 ) else (
     echo test_all.exe が見つかりませんでした。
     cd "%ORIG_DIR%"
